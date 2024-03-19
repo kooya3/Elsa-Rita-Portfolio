a9c4cea142ec292d19daf05ef5b7ec39fc7c9ce9
Elyees Tatua <elyeestatua@gmail.com>
2024-03-19 23:01:45 +0300

    ops(update)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,18 @@ const Home: NextPage = () => {
       </Head>
 
       <h1 className="p-text-red-500">Let's build something Awesome</h1>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <img
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="https://i.imgur.com/e2yvD6A.png"
+              alt=""
+            />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
