f8df4949fe358495638d1b315aaabf175bea020c
Elyees Tatua <elyeestatua@gmail.com>
2024-03-19 23:03:51 +0300

    ops(update)

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,6 @@ const Home: NextPage = () => {
         <footer className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
             <img
-              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
-              src="https://i.imgur.com/e2yvD6A.png"
 
     </div>
   );
