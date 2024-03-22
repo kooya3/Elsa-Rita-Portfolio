4b754123528a005cd5969544ac56aabdf55700b7
Elyees Tatua <elyeestatua@gmail.com>
2024-03-22 23:48:00 +0300

    chore(header): Updating the header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className="sticky top-0"> 
+    <header className="sticky top-0 flex items-start"> 
       <div className="flex flex-row items-center">
         {/* Social Icons */}
         <SocialIcon
